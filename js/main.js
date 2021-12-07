aaaae4381a55db53efe6d0195621662fe61678be
Sel-K <61660655+Sel-K@users.noreply.github.com>
2021-12-07 23:26:10 +0300

    create: add modal & fix media footer

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
-var hotelSlider = new Swiper('.hotel-slider', {
+$(document).ready(function(){
+  var hotelSlider = new Swiper('.hotel-slider', {
     loop: true,
     navigation: {
       nextEl: '.hotel-slider__button--next',
@@ -31,3 +32,25 @@ var hotelSlider = new Swiper('.hotel-slider', {
     console.log('clik!!');
     document.querySelector('.navbar-bottom').classList.toggle('navbar-bottom--visible')
   });
+
+  var modalButton = $("[data-toggle=modal]");
+  var closeModalButton = $(".modal__close");
+    modalButton.on("click", openModal);
+    closeModalButton.on("click", closeModal);
+
+  function openModal() {
+    var modalOverlay = $(".modal__overlay");
+    var modalDialog = $(".modal__dialog");
+    modalOverlay.addClass('modal__overlay--visible');
+    modalDialog.addClass('modal__dialog--visible');
+  }
+  
+  function closeModal(event) {
+    event.preventDefault();
+    var modalOverlay = $(".modal__overlay");
+    var modalDialog = $(".modal__dialog");
+    modalOverlay.removeClass('modal__overlay--visible');
+    modalDialog.removeClass('modal__dialog--visible');
+  }
+
+});
